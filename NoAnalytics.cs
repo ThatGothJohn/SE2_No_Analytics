f038405173a4f719760d743beed953a242bf2274
ThatGothJohn <thatbotjohn@gmail.com>
2025-02-16 02:31:02 +0000

    First External Commit

---
+++ NoAnalytics.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Keen.Game2.Game.Plugins;
+using Keen.VRage.Library.Diagnostics;
+using System.Reflection;
+
+namespace NoAnalytics
+{
+    public class Plugin : IPlugin
+    {
+        public const string Name = "SE2 No Analytics";
+
+        public Plugin()
+        {
+            Log.Default.WriteLine($"Loaded {Name} Plugin!");
+
+            Harmony harmony = new(Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log.Default.WriteLine($"Applied {Name} Patches!");
+        }
+    }
+}
---
+++ Patches/patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Keen.VRage.Core.Extensions;
+using Keen.VRage.Core;
+using Keen.VRage.Core.Analytics;
+
+namespace NoAnalytics.Patches
+{
+    [HarmonyPatch(typeof(EngineBuilderExtensions), nameof(EngineBuilderExtensions.AddElasticAnalyticsReporter))]
+    internal class EngineBuilderExtensions_AddElasticAnalyticsReporter_Patch
+    {
+        private static bool Prefix(ref EngineBuilder builder, ref ElasticAnalyticsBuilder ob, ref EngineBuilder __result)
+        {
+            __result = builder;
+            return false;            
+        }
+    }
+
+}
